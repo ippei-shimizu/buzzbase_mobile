024ebf6c2f2e3e422374ed180620430fb20e5492
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2026-04-03 00:39:17 +0900

    Fix: import順序のlint警告を修正

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,8 @@
 import "../global.css";
-import { useEffect } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 import { usePushNotifications } from "@hooks/usePushNotifications";
 import { useStoreReview } from "@hooks/useStoreReview";
 import { configureGoogleSignIn } from "@services/googleAuthService";
